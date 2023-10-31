c8fd9d4b6ea9d7292a5d1109dcf80e99ef360d4d
Armenteros05 <sarmerod@myuax.com>
2023-10-31 11:27:06 +0100

    Merge remote-tracking branch 'origin/Armenteros' into Armenteros

--- tiny-lisp.cpp
+++ tiny-lisp.cpp
@@ -1,54 +1,77 @@
 
 #include <iostream>
-#include <sstream>
-#include <stack>
-#include <vector>
-
-typedef std::vector<std::string> Tokens;
-
-Tokens tokenize(const std::string& input) {
-    Tokens tokens;
-    std::string token;
-    std::istringstream stream(input);
-
-    while (stream >> token) {
-        tokens.push_back(token);
-    }
-
-    return tokens;
-}
-
-int evaluate(Tokens& tokens) {
-    std::stack<int> stack;
-
-    while (!tokens.empty()) {
-        std::string token = tokens.back();
-        tokens.pop_back();
-
-        if (token == "+") {
-            int a = stack.top();
-            stack.pop();
-            int b = stack.top();
-            stack.pop();
-            stack.push(a + b);
-        } else if (token == "*") {
-            int a = stack.top();
-            stack.pop();
-            int b = stack.top();
-            stack.pop();
-            stack.push(a * b);
-        } else {
-            stack.push(std::stoi(token));
-        }
-    }
-
-    return stack.top();
-}
+using namespace std;
 
 int main() {
-    std::string input;
-    std::getline(std::cin, input);
-    Tokens tokens = tokenize(input);
-    std::cout << evaluate(tokens) << std::endl;
+    //añadimos el operador de suma
+    int a;
+    int b;
+    cout << "Ingrese el primer numero: ";
+    cin >> a;
+    cout << "Ingrese el segundo numero: ";
+    cin >> b;
+    int suma;
+    suma= a+b;
+    cout << "La suma es: " << suma << endl;
+    //añadimos el operador resta
+cout << "Ahora vamos a hacer una resta ";
+int c;
+int d;
+cout << endl;
+cout << "Ingrese el primer numero: ";
+cin >> c;
+cout << "Ingrese el segundo numero: ";
+cin >> d;
+int resta;
+resta= c-d;
+cout << "La resta es: " << resta << endl;
+//añadimos el operador multiplicacion
+cout << "Ahora vamos a hacer una multiplicacion ";
+int e;
+int f;
+cout << endl;
+cout << "Ingrese el primer numero: ";
+cin >> e;
+cout << "Ingrese el segundo numero: ";
+cin >> f;
+int multiplicacion;
+multiplicacion= e*f;
+cout << "La multiplicacion es: " << multiplicacion << endl;
+//añadimos el operador division
+cout << "Ahora vamos a hacer una division ";
+int g;
+int h;
+cout << endl;
+cout << "Ingrese el primer numero: ";
+cin >> g;
+cout << "Ingrese el segundo numero: ";
+cin >> h;
+int division;
+division= g/h;
+cout << "La division es: " << division << endl;
+//añadimos el operador modulo
+cout << "Ahora vamos a hacer un modulo ";
+int i;
+int j;
+cout << endl;
+cout << "Ingrese el primer numero: ";
+cin >> i;
+cout << "Ingrese el segundo numero: ";
+cin >> j;
+int modulo;
+modulo= i%j;
+cout << "El modulo es: " << modulo << endl;
+//añadimos el operador potencia
+cout << "Ahora vamos a hacer una potencia ";
+int k;
+int l;
+cout << endl;
+cout << "Ingrese el primer numero: ";
+cin >> k;
+cout << "Ingrese el segundo numero: ";
+cin >> l;
+int potencia;
+potencia= k^l;
+cout << "La potencia es: " << potencia << endl;
     return 0;
 }
